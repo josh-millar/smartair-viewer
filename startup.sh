28e6b7ebf7c14a38ea0332c841eb76adee565104
j-millar <86832312+j-millar@users.noreply.github.com>
2022-04-01 15:51:43 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,4 +7,4 @@ apt-get install -y libx11-6
 apt-get install -y libsm6 
 apt-get install -y libxext6 
 apt-get install -y libgl1-mesa-dev 
-apt-get install -y libxrender1 
+# apt-get install -y libxrender1 
