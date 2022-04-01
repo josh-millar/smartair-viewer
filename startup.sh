18e60f12f848a76b3ad5323259b89ebd6cac2470
josh-millar <joshua.millar@wsp.com>
2022-04-01 15:16:03 +0100

    fixing startup script

--- startup.sh
+++ startup.sh
@@ -2,11 +2,11 @@
 
 apt-get update
 apt-get upgrade
-apt-get install libx11-dev 
-apt-get install libx11-6 
-apt-get install libsm6 
-apt-get install libxext6 
-apt-get install libgl1-mesa-dev 
-apt-get install libxrender1 
+apt-get install -y libx11-dev 
+apt-get install -y libx11-6 
+apt-get install -y libsm6 
+apt-get install -y libxext6 
+apt-get install -y libgl1-mesa-dev 
+apt-get install -y libxrender1 
 
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
